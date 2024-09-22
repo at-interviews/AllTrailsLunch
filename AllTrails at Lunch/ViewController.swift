ecf0403a0e897407f56acba408d08ecc029a6d36
Josh <curt.prickle0i@icloud.com>
2024-09-22 13:35:34 -0400

    Initial Commit

---
+++ AllTrails at Lunch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllTrails at Lunch
+//
+//  Created by Josh Lang on 9/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
